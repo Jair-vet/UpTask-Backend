96f68b1cba22f4849447472ea0baedfe39a26af3
Jair <jairaceves56@gmail.com>
2024-05-23 17:43:53 -0600

    Obtenemos un proyecto por ID

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -28,6 +28,22 @@ export class ProjectController {
         }
     }
 
+    static getProjectById = async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        try {
+            const project = await Project.findById( id )
+            if(!project){
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({ error: error.message})
+            }
+            res.json({data: project})
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
 
 }
