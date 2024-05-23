ba0ba5545677bcb365294406d3c2f61007842490
Jair <jairaceves56@gmail.com>
2024-05-23 17:31:30 -0600

    Validamos desde el Servidor usando un middleware

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router()
 
-router.post('/', ProjectController.createProject)
+router.post('/',
+
+    body('projectName')
+        .notEmpty().withMessage('El nombre del Proyecto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripcion del proyecto es Obligatoria'),
+    handleInputErrors,
+    // Controlador
+    ProjectController.createProject
+)
 router.get('/', ProjectController.getAllProjects)
 
 
