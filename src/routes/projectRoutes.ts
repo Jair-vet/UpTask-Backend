b66ca056bd741568ee71bf7481aba1b17260a5dd
Jair <jairaceves56@gmail.com>
2024-05-23 18:07:49 -0600

    Actualizar y Eliminar Proyectos

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -4,9 +4,8 @@ import { ProjectController } from '../controllers/ProjectController';
 import { handleInputErrors } from '../middleware/validation';
 
 const router = Router()
-
+// ! CREAR
 router.post('/',
-
     body('projectName')
         .notEmpty().withMessage('El nombre del Proyecto es Obligatorio'),
     body('clientName')
@@ -17,14 +16,36 @@ router.post('/',
     // Controlador
     ProjectController.createProject
 )
-
+//  ! Obtener
 router.get('/', ProjectController.getAllProjects)
 
+// ! Obtener by ID
 router.get('/:id',
     param('id').isMongoId().withMessage('ID no válido'),
     handleInputErrors,
     // controlador
     ProjectController.getProjectById
+)
+
+// ! Actulizar
+router.put('/:id',
+    param('id').isMongoId().withMessage('ID no válido'),
+    body('projectName')
+        .notEmpty().withMessage('El nombre del Proyecto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripcion del proyecto es Obligatoria'),
+    handleInputErrors,
+    // controlador
+    ProjectController.updateProject
+)
+// ! Eliminar
+router.delete('/:id',
+    param('id').isMongoId().withMessage('ID no válido'),
+    handleInputErrors,
+    // controlador
+    ProjectController.deleteProject
 )
 
 
