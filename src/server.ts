cb6ed4ba2fb49f2579afd37daa29a49a9c152927
Jair <jairaceves56@gmail.com>
2024-06-06 17:50:14 -0600

    Agregamos Morgan al Proyecto

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { connectDB } from './config/db'
 import router from './routes/projectRoutes'
 import { corsConfig } from './config/cors'
 import cors from 'cors';
+import morgan from 'morgan'
 
 dotenv.config()
 
@@ -15,6 +16,9 @@ const server = express()
 
 // Permitir conexiones CORS
 server.use(cors(corsConfig))
+
+// Login
+server.use(morgan('dev'))
 
 // Leer datos de formularios de tipo JSON en el servidor
 server.use(express.json())
