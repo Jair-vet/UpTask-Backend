cdb73e6b0972ffa606157e697f3e14e2a027a464
Jair <jairaceves56@gmail.com>
2024-06-11 19:05:59 -0600

    Creamos el Modelo de Notas

--- src/models/Note.ts
+++ src/models/Note.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, Document, Types } from 'mongoose'
 
 export interface INote extends Document {
     content: string
-    createdBy: Types.ObjectId
+    // createdBy: Types.ObjectId
     task: Types.ObjectId
 }
 
@@ -11,11 +11,11 @@ const NoteSchema: Schema = new Schema({
         type: String,
         required: true
     },
-    createdBy: {
-        type: Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
+    // createdBy: {
+    //     type: Types.ObjectId,
+    //     ref: 'User',
+    //     required: true
+    // },
     task: {
         type: Types.ObjectId,
         ref: 'Task',
